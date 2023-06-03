6af892f1b7220a364fa5f3ca61e4b3186358f2c0
Pedro Andreotti S <pedroandreottijetforce@gmail.com>
2023-06-02 22:11:53 -0300

    Entrada validada B)

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿int numero = -1;
+Console.WriteLine("---Entrada válida---");
+
+while (numero < 0 || numero > 9)
+{
+    Console.WriteLine("Digite somente números entre 1 e 9 (0 PARA CANCELAR)");
+    numero = Convert.ToInt32(Console.ReadLine());
+}
+
+if (numero == 0)
+{
+    Console.WriteLine("CPF CANCELADO COM SUCESSO");
+}
+
+else
+{
+    Console.WriteLine($"\nNúmero selecionado = {numero}");
+}
+
+Console.WriteLine("\nVolte sempre!");
